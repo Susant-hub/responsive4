72587bdcf3916bf7ea6e4f3984fabc3995f14c19
Tech_sus <subedisusant632@gmail.com>
2025-12-15 21:37:00 +0545

    Added: styling for articles, footer section and mobile navigation menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './scss/main.scss'
+
+const mobileMenu = document.querySelector('.mobile-menu')
+const primaryNav = document.querySelector('.primary-navigation')
+const navbar = document.querySelector('.nav-wrapper')
+const hamburger = document.querySelector('.hamburger-icon')
+const close = document.querySelector('.close-icon')
+
+
+mobileMenu.addEventListener('click', ()=>{
+    primaryNav.classList.toggle('show')
+    hamburger.classList.toggle('hide')
+    close.classList.toggle('show')
+    navbar.toggleAttribute('data-overlay')
+})
